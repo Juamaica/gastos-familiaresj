8e050e62508dd7401e33a679c5853b7b13f0d04a
Ulices-byte <159735603+Juamaica@users.noreply.github.com>
2026-08-13 21:23:51 -0400

    Update supabaseClient.js

--- supabaseClient.js
+++ supabaseClient.js
@@ -8,7 +8,7 @@
 // =========================================================
 
 (function () {
-  const SUPABASE_URL = "sb_publishable_K8NMIfm6yNlfR5N2C8h8yw_0DVsA7W-";
+  const SUPABASE_URL = "https://cdgdxxafylyoydvhplhq.supabase.co";
   const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNkZ2R4eGFmeWx5b3lkdmhwbGhxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODY2NjQ3NjIsImV4cCI6MjEwMjI0MDc2Mn0.Dgdz9s1N4sJ-8IW8R_md2HqnXa9pfQthwP0uxZYqY5s";
 
   window.supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
